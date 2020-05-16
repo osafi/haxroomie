f9dfcc2702be6a9fd9fe9b50c3aca7e967f03032
Omeed Safi <omeed@safi.ms>
2020-05-16 00:16:53 -0700

    Initial commit

---
+++ root/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -f /tmp/haxroomie.lock
+mkdir -p /root/.haxroomie
+if [ ! -f /root/.haxroomie/config.js ]; then
+  echo "$HAXROOMIE_CONFIG" > /root/.haxroomie/config.js
+  cat /root/.haxroomie/config.js
+fi
+
+haxroomie-cli --no-sandbox
